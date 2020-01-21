5cc7e3160c0d57a822e9a5de3dc54ab46447a45b
95twan <95twan@naver.com>
2020-01-21 21:12:40 +0900

    add user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=45)
+    password = models.CharField(max_length=45)
+    nickname = models.CharField(max_length=45)
+    email = models.CharField(max_length=45, blank=True, null=True)
+    registered_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        managed = False
+        db_table = 'user'

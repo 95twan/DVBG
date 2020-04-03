d767d9557cf36a49ddabd873acb9553ebd46d8b0
95twan <95twan@naver.com>
2020-04-04 02:49:11 +0900

    bug fix

--- DVBG/settings.py
+++ DVBG/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.180.81.200', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['15.165.206.181', '127.0.0.1', 'localhost']
 
 
 # Application definition
